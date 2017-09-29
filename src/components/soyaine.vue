85e0e85bdad741ea6af901c6c1bad25e11503e06
soyaine <soyaine@outlook.com>
2017-09-29 16:26:45 +0800

    router

---
+++ src/components/soyaine.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="soyaine">
+    <h1>Soyaine article</h1>
+    <ul>
+      <li v-for="item in list">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'soyaine',
+  data () {
+    return {
+      mag: 'soyaine',
+      list: [
+        '第一篇文章',
+        '公众号的文章',
+        '简书文章'
+      ]
+    }
+  }
+}
+</script>
+
+<style media="screen" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import soyaine from '@/components/soyaine'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/soyaine',
+      name: 'Soyaine',
+      component: soyaine
     }
   ]
 })
